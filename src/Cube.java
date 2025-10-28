0fcee18db95b0cd430052dbc6e4f77262a994bb3
Ansonlin <ansonlin2411@gmail.com>
2025-10-28 18:25:20 +0800

    bobthebuilder

---
+++ src/Cube.java
@@ -0,0 +1,33 @@
+public class Cube {
+    int side;
+
+    public Cube() {
+        this.side = 1;
+    }
+
+    public Cube(int cubeLength) {
+        if (cubeLength < 1) {
+            throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
+        }
+        this.side = cubeLength;
+    }
+
+    public void setSide(int newSide) {
+        if (newSide < 1) {
+            throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
+        }
+        this.side = newSide;
+    }
+
+    public int getSide() {
+        return this.side;
+    }
+
+    public int calculateVolume() {
+        return side * side * side;
+    }
+
+    public int calculateSurfaceArea() {
+        return side * side * 6;
+    }
+}
